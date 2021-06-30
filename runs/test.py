f2ea97650ba34cf238ac650f32578eaa4a0a5e47
Boyuan Long <boyuanlo@usc.edu>
2021-06-29 21:49:31 -0700

    add example

---
+++ runs/test.py
@@ -0,0 +1,22 @@
+
+from runner.run_description import RunDescription, Experiment, ParamGrid
+
+_params = ParamGrid([
+    ('l', ['runs', 'slurm']),
+])
+
+_experiments = [
+    Experiment(
+        'test',
+        'ls',
+        _params.generate_params(randomize=False),
+    ),
+    Experiment(
+        'test2',
+        'ls',
+        _params.generate_params(randomize=False),
+    )
+]
+
+
+RUN_DESCRIPTION = RunDescription('test', experiments=_experiments)
